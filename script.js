ee49b2129da2c2239edcbd0d2324a03615e1d73e
sandy <sandeep620670@gmail.com>
2026-01-23 13:56:38 +0530

    fix note

--- script.js
+++ script.js
@@ -778,6 +778,7 @@ document.getElementById("entryForm").addEventListener("submit", async (e) => {
         reg: document.getElementById("reg").value,
         mobile: document.getElementById("mobile").value,
         email: document.getElementById("email").value,
+        note: document.getElementById("note").value,
         lat: pos.coords.latitude,
         lon: pos.coords.longitude,
         deviceId: getDeviceId(),
